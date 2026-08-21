c53fabcb85bb4912a33bd55dab93e7ea0863471f
shahinsheikh <shahin.taehyun@gmail.com>
2026-08-21 12:43:49 +0400

    fix init import error

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,10 +2,12 @@ from app.routers.catalog import router as catalog_router
 from app.routers.google_auth import router as google_auth_router
 from app.routers.health import router as health_router
 from app.routers.twilio import router as twilio_router
+from app.routers.legal import router as legal_router
 
 __all__ = [
     "catalog_router",
     "google_auth_router",
     "health_router",
     "twilio_router",
+    "legal_router"
 ]
